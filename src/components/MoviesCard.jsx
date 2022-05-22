8b990fe02b2c6f35f0c170677f6f5e69a6b61054
all1son4 <dm.strok98@gmail.com>
2022-05-22 12:45:13 +0300

    fix: fixed after first review

--- src/components/MoviesCard.jsx
+++ src/components/MoviesCard.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import moviePic from '../images/card__picture.png'
+import {useLocation} from "react-router-dom";
 
 function MoviesCard() {
 
+  const location = useLocation();
+
   const saveFilmHandle = (event) => {
     event.preventDefault()
-    console.log(1)
     event.target.classList.add('card__button_active')
     event.target.textContent = ''
+  }
+
+  const deleteFilmHandle = (event) => {
+    event.preventDefault()
   }
 
   return (
@@ -18,7 +24,9 @@ function MoviesCard() {
           <p className="card__duration">27 минут</p>
         </div>
         <img className="card__image" src={moviePic} alt="Movie title"/>
-        <button className="card__button" type={'button'} onClick={saveFilmHandle}>Сохранить</button>
+        {location.pathname === '/movies'
+          ? <button className="card__button" type={'button'} onClick={saveFilmHandle}>Сохранить</button>
+          : <button className="card__button card__button_delete" type={'button'} onClick={deleteFilmHandle}/>}
       </article>
 
     </>
